7b3fdfc03d0102bdba9969598ac0051a80ac8940
yu zhou <384986004@qq.com>
2020-07-18 15:01:33 +0800

    题目16 解法2 双层循环 双指针

--- Algorithm.playground/Pages/LeetCode 16.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/LeetCode 16.xcplaygroundpage/Contents.swift
@@ -69,6 +69,41 @@ class Solution {
         }
         return r ?? 0
     }
+    //双层循环 双指针 参照题15 满足时间要求
+    func threeSumClosest1(_ nums: [Int], _ target: Int) -> Int {
+        var r: Int!
+        let numsSorted = nums.sorted()
+        for i in 0..<numsSorted.count-2 {
+            if i-1 >= 0 && numsSorted[i] == numsSorted[i-1] {
+                continue
+            }
+            let sub = target-numsSorted[i]
+            var left = i+1
+            var right = numsSorted.count-1
+//            var diff = abs(numsSorted[left]+numsSorted[right]-sub)
+            while left<right {
+                if numsSorted[left]+numsSorted[right] == sub {
+                    return sub+numsSorted[i]
+                } else {
+//                    print(abs(numsSorted[left]+numsSorted[right]-sub),diff,"dddd")
+                    if r == nil {
+                        r = (numsSorted[i]+numsSorted[left]+numsSorted[right])
+                    }
+                    if abs(numsSorted[left]+numsSorted[right]-sub) <= abs(r-target) {
+                        r = (numsSorted[i]+numsSorted[left]+numsSorted[right])
+//                        diff = abs(numsSorted[left]+numsSorted[right]-sub)
+                    }
+                    if numsSorted[left]+numsSorted[right] < sub {
+                        left += 1
+                    } else {
+                        right -= 1
+                    }
+                }
+            }
+        }
+        return r ?? 0
+    }
 }
-
-Solution().threeSumClosest([1,1,1,0],-100)
+let dd = [47,-48,-72,97,-78,50,-22,18,9,24,28,-53,44,-96,50,45,86,11,21,-44,67,83,55,-86,-33,0,-53,-94,-60,57,-72,-73,-27,13,91,80,18,-80,-29,-69,-74,-90,54,22,3,91,-47,-32,80,-55,69,-95,62,-92,4,-86,62,3,23,-30,-4,0,49,24,10,-32,79,-99,-66,-30,-83,-13,90,-27,9,-4,9,98,-70,-19,32,24,-77,83,11,-78,-94,4,41,61,20,96,-36,54,-46,-51,91,54,30,-42,82,0,9,24,-2,32,-16,-18,87,23,78,-10,-82,-67,68,-18,-61,91,-90,-53,67,-48,12,1,-71,-99,31,82,39,-56,23,-89,-58,19,-60,39,-23,-76,-85,67,-33,69,-74,-8,-99,52,-70,-71,85,-8,28,-3,-100,18,88,5,-16,17,91,-35,22,-76]
+//[-1,2,1,-4]
+Solution().threeSumClosest1([1,1,1,0],-100)
