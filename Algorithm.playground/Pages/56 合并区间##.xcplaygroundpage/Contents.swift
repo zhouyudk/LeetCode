7f158f94f201a349d630f57080f9e98c88c69959
yu zhou <yu.zhou@razer.com>
2020-09-07 18:30:17 +0800

    合并区间

--- Algorithm.playground/Pages/56 合并区间##.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/56 合并区间##.xcplaygroundpage/Contents.swift
@@ -7,24 +7,17 @@ import Foundation
 class Solution {
     func merge(_ intervals: [[Int]]) -> [[Int]] {
         guard intervals.count>1 else { return intervals}
+        let intervals = intervals.sorted(by: { $0[0] < $1[0] })
         var res = [intervals.first!]
         for i in 1..<intervals.count {
             let range = intervals[i]
             for j in 0..<res.count {
                 var r = res[j]
-                if range[1]+1<r[0] || range[0]-1>r[1] {
+                if range[1]<r[0] || range[0]>r[1] {
                     if j+1 >= res.count {
                         res.append(range)
                     }
-                } else if  range[1]+1 == r[0] {
-                    r[0] = range[0]
-                    res[j] = r
-                    break
-                } else if range[0]-1 == r[1] {
-                    r[1] = range[1]
-                    res[j] = r
-                    break
-                }else if range[0]>=r[0] {
+                } else if range[0]>=r[0] {
                     if range[1]>r[1] {
                         r[1] = range[1]
                         res[j] = r
